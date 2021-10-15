19e91d9fd197b5a0f000012488c9b32e4556e98f
Florencia Belén De Souza <90360929+PuchikoNyu@users.noreply.github.com>
2021-10-15 01:09:04 -0300

    proceso

--- archivo.js
+++ archivo.js
@@ -1,1 +1,24 @@
-//Actividad 1 Clase 4
+//Actividad 1 Clase 4 - 
+
+alert("Ejercicio 1")
+console.log("Ejercicio 1")
+
+//Actividad 2 Clase 4 - 
+
+alert("Ejercicio 2")
+console.log("Ejercicio 2")
+
+//Actividad 3 Clase 4 - 
+
+alert("Ejercicio 3")
+console.log("Ejercicio 3")
+
+//Actividad 4 Clase 4 - 
+
+alert("Ejercicio 4")
+console.log("Ejercicio 4")
+
+//Actividad 5 Clase 4 - 
+
+alert("Ejercicio 5")
+console.log("Ejercicio 5")
