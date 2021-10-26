3628d61b7b0cc6779cc21d24f0864df577faa97d
Florencia Belén De Souza <90360929+PuchikoNyu@users.noreply.github.com>
2021-10-26 00:26:35 -0300

    primer pre entrega corrección

--- scripts/archivo.js
+++ scripts/archivo.js
@@ -22,6 +22,16 @@ class Productos {
                 console.log("La solicitud del usuario supera el stock disponible.")
             }
         }
+        this.restarStock = (cantidadResta) => {
+            cantidadResta = parseInt(cantidadResta)
+            if (this.stock >= cantidadResta) {
+                this.stock -= cantidadResta
+                console.log("STOCK DISPONIBLE")
+            } else {
+                alert("STOCK INSUFICIENTE")
+                console.log("STOCK INSUFICIENTE")
+            }
+        }
     }
 }
 
@@ -34,7 +44,7 @@ productos.push(new Productos(3, "pantalon", "short", "negro", 700, 20))
 productos.push(new Productos(4, "pantalon", "calza", "negro", 1000, 20))
 productos.push(new Productos(5, "botella", "caramañola plastico", "transparente", 500, 20))
 productos.push(new Productos(6, "botella", "caramañola aluminio", "aluminio", 700, 20))
-productos.push(new Productos(7,"estuche", "porta celular", "gris", 300, 10))
+productos.push(new Productos(7, "estuche", "porta celular", "gris", 300, 10))
 
 //////
 
@@ -50,7 +60,7 @@ function unidad() {
 
     let unidades = parseFloat(prompt("Ingrese las unidades que desea: (0 a 5)"))
 
-    while (unidades != "ESC") {
+    while (unidades != "") {
 
         if (unidades >= 0 && unidades <= 5) {
             console.log("El usuario ingreso: " + unidades + ".")
@@ -77,77 +87,37 @@ function seleccionSuma() {
     let total = 0
     let unidades = 0
     let suma = 0
-    let ingreso = prompt("Ingrese un item por vez, respetando el orden:     1)musculosa     2)manga corta       3)bermuda       4)short      5)calza     6)caramañola plastico      7)caramañola aluminio      8)porta celular      FINALICE CON ENTER")
+    let ingreso = prompt("Ingrese un item por vez:     1)musculosa     2)manga corta       3)bermuda       4)short      5)calza     6)caramañola plastico      7)caramañola aluminio      8)porta celular       FINALICE CON ESC")
     console.log("El usuario ingresó: " + ingreso + ".")
 
-    //
+    while (ingreso != "ESC") {
 
-    productos.forEach(item => {
-        if (ingreso === item.nombre) {
-            unidades = unidad()
-            total = multiplicar(item.precio, unidades)
-            suma += total
-            item.verStock(unidades)
-            console.log("Total: $" + total + " por " + unidades + " de " + ingreso + ". Total por todo: $" + suma + ".")
-        } else {
-            alert("Ingreso NO valido")
-            console.log("El usuario realizó un ingreso erróneo.")
-            console.log("El usuario lleva sumado: $" + suma + ".")
-        }
+        productos.forEach(item => {
 
-        ingreso = prompt("Ingrese un item por vez, respetando el orden:     1)musculosa     2)manga corta       3)bermuda       4)short      5)calza     6)caramañola plastico      7)caramañola aluminio      8)porta celular      FINALICE CON ENTER")
+            if (ingreso === item.nombre) {
+
+                unidades = unidad()
+                item.verStock(unidades)
+                item.restarStock(unidades)
+                if (unidades <= item.stock) {
+                    total = multiplicar(item.precio, unidades)
+                    suma += total
+                }
+                console.log("Total: $" + total + " por " + unidades + " de " + ingreso + ". Total por todo: $" + suma + ".")
+                return
+            }
+        })
+        console.log("El usuario lleva sumado: $" + suma + ".")
+        ingreso = prompt("Ingrese un item por vez:     1)musculosa     2)manga corta       3)bermuda       4)short      5)calza     6)caramañola plastico      7)caramañola aluminio      8)porta celular       FINALICE CON ESC")
         console.log("El usuario ingresó: " + ingreso + ".")
-    })
+    }
     console.log("El usuario sumó en total: $" + suma + ".")
     return suma
 }
 
 /////
 
-function ordenamiento() {
-    let filtro = prompt("Listar productos disponibles: ( id / nombre / precio )")
-
-    if (filtro === "nombre") {
-
-        const filtroNombre = productos.slice().sort((a, b) => {
-            if (a.nombre > b.nombre) {
-                return 1
-            } else if (a.nombre < b.nombre) {
-                return -1
-            } else {
-                return 0
-            }
-        })
-
-        console.log(filtroNombre)
-        alert("ORDEN CORRECTO")
-
-    } else if (filtro === "precio") {
-
-        const filtroPrecio = productos.slice().sort((a, b) => a.precio - b.precio)
-
-        console.log(filtroPrecio)
-        alert("ORDEN CORRECTO")
-
-    } else if (filtro === "id") {
-
-        const filtroOrden = productos.slice().sort((a, b) => a.id - b.id)
-
-        console.log(filtroOrden)
-        alert("ORDEN CORRECTO")
-
-    } else {
-
-        alert("ERROR DE INGRESO")
-    }
-
-}
-
-/////
-
-alert("Bienvenido!")
-ordenamiento()
-alert("A continuación puede seleccionar los productos que desea adquirir como complemento de la carrera: ")
+alert("Bienvenido! A continuación puede seleccionar los productos que desea adquirir como complemento de la carrera: ")
 subtotal = seleccionSuma()
 total = multiplicar(subtotal, iva)
 resultado()
