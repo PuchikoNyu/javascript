496e71590496025a9e3f3e95c58eeb4a5b77cbbc
Florencia Belén De Souza <90360929+PuchikoNyu@users.noreply.github.com>
2021-11-22 22:12:16 -0300

    avance

--- scripts/busqueda.js
+++ scripts/busqueda.js
@@ -1,23 +1,30 @@
 const formulario = document.querySelector("#formulario");
 const boton = document.querySelector("#boton");
-const resultado = document.querySelector("#resultado")
+// const resultado = document.querySelector("#resultado")
 
 const filtrar = () => {
 
-    resultado.innerHTML = '';
+    contenedorProductos.innerHTML = '';
 
     const texto = formulario.value.toLowerCase();
     for (let producto of stockProductos) {
         let nombre = producto.nombre.toLowerCase();
         if (nombre.indexOf(texto) !== -1) {
-            resultado.innerHTML += `
-            <li class="my-2">${producto.nombre} - Valor: $${producto.precio}</li>
-            `
+            const div = document.createElement('div')
+            div.className = "col-3 my-3 py-3 mx-3 border rounded d-flex flex-column justify-content-between"
+            div.innerHTML = `
+                    <img src=${producto.img} alt="${producto.nombre}" class="img-thumbnail">
+                    <h3>${producto.nombre}</h3>
+                    <p>Tipo: ${producto.tipo}</p>
+                    <p class="precioProducto">Precio: $${producto.precio}</p>
+                    <button onclick="agregarAlCarrito(${producto.id})" class="agregar-carrito rounded">Agregar <i class="fas fa-shopping-cart"></i></button>
+        `
+            contenedorProductos.append(div)
         }
     }
 
-    if (resultado.innerHTML === '') {
-        resultado.innerHTML += `
+    if (contenedorProductos.innerHTML === '') {
+        contenedorProductos.innerHTML += `
     <li> Producto no encontrado...</li>
     `
     }
--- scripts/stock.js
+++ scripts/stock.js
@@ -1,5 +1,5 @@
 class Producto {
-    constructor(id, categoria, subcategoria, variedad, tipo, nombre, precio, img) {
+    constructor(id, categoria, subcategoria, variedad, tipo, nombre, precio, img, stock) {
         this.id = id
         this.categoria = categoria
         this.subcategoria = subcategoria
@@ -8,42 +8,42 @@ class Producto {
         this.nombre = nombre
         this.precio = precio
         this.img = img
+        this.stock = stock
     }
 }
-const stockProductos = []
-stockProductos.push(new Producto(1, "facial", "aguas", "aplicador", "rosas", "Agua de Rosas", 700, './media/aRosas.jpeg'))
-stockProductos.push(new Producto(2, "facial", "aguas", "aplicador", "hamamelis", "Agua de Hamamelis", 750, './media/aHamamelis.jpeg'))
-stockProductos.push(new Producto(3, "facial", "aguas", "aplicador", "azahar", "Agua de Azahar", 700, './media/aAzahar.jpeg'))
-stockProductos.push(new Producto(4, "capilar", "shampoo", "solido", "seco", "Shampoo Sólido para Cabello Seco", 900, './media/seco.jpeg'))
-stockProductos.push(new Producto(5, "capilar", "shampoo", "solido", "fortalecedor", "Shampoo Sólido Fortaleceer", 900, './media/fortalecedor.jpeg'))
-stockProductos.push(new Producto(6, "capilar", "shampoo", "solido", "equilibrado", "Shampoo Sólido Equilibrado", 900, './media/equilibrado.jpeg'))
-stockProductos.push(new Producto(7, "capilar", "shampoo", "solido", "graso", "Shampoo Sólido para Cabello Graso y Anti-Caspa", 900, './media/graso.jpeg'))
-stockProductos.push(new Producto(8, "capilar", "acondicionador", "solido", "acondicionador", "Acondicionador Sólido", 1000, './media/acondicionador.jpeg'))
-stockProductos.push(new Producto(9, "capilar", "serum", "aplicador", "serum", "Serum Capilar", 1100, './media/serum.jpeg'))
-stockProductos.push(new Producto(10, "corporal", "crema", "manos", "nutritiva", "Crema para Manos Nutritiva", 700, './media/creMan.jpeg'))
-stockProductos.push(new Producto(11, "corporal", "crema", "manos", "extra nutritiva", "Crema para Manos Extra-Nutritiva", 750, './media/creMan.jpeg'))
-stockProductos.push(new Producto(12, "corporal", "crema", "cuerpo", "nutritiva", "Crema Corporal Nutritiva", 1250, './media/creCor.jpeg'))
-stockProductos.push(new Producto(13, "corporal", "crema", "cuerpo", "extra nutritiva", "Crema Corporal Extra-Nutritiva", 1350, './media/creCorN.jpeg'))
-stockProductos.push(new Producto(14, "corporal", "jabon", "exfoliante", "mentolado", "Jabón Exfoliante Mentolado", 650, './media/jabonExf.jpeg'))
-stockProductos.push(new Producto(14, "corporal", "jabon", "esenciales", "tilo", "Jabón Esencial de Tilo", 400, './media/jabones.jpeg'))
-stockProductos.push(new Producto(15, "corporal", "jabon", "esenciales", "oleo 31", "Jabón Esencial de Oleo 31", 400, './media/jabones.jpeg'))
-stockProductos.push(new Producto(16, "corporal", "jabon", "esenciales", "coco", "Jabón Esencial de Coco", 400, './media/jabones.jpeg'))
-stockProductos.push(new Producto(17, "corporal", "jabon", "especiales", "calendula", "Jabón Esencial de Caléndula", 550, './media/jabCal.jpeg'))
-stockProductos.push(new Producto(18, "corporal", "jabon", "especiales", "jazmin", "Jabón Esencial de Jazmín", 5500, './media/jabJaz.jpeg'))
-stockProductos.push(new Producto(19, "corporal", "pasta dental", "vidrio", "eucalipto", "Pasta dental de Eucalipto", 550, './media/pastaDental.jpeg'))
-stockProductos.push(new Producto(20, "corporal", "pasta dental", "vidrio", "citrico", "Pasta dental Cítrica", 550, './media/pastaDental.jpeg'))
-stockProductos.push(new Producto(21, "corporal", "desodorante", "vidrio", "mix foral", "Desodorante Mix Floral", 600, './media/desodorante.jpeg'))
-stockProductos.push(new Producto(22, "corporal", "desodorante", "vidrio", "lavanda", "Desodorante Lavanda", 600, './media/desodorante.jpeg'))
-stockProductos.push(new Producto(23, "accesorios", "peines", "madera", "simple", "Peine para Cabello Varios Modelos Simples", 650, './media/peineMadera.jpeg'))
-stockProductos.push(new Producto(24, "accesorios", "peines", "madera", "cabello seco", "Peine para Cabello Seco", 900, './media/peineSeco.jpeg'))
-stockProductos.push(new Producto(25, "accesorios", "peines", "madera", "cabello humedo", "Peine para Cabello Húmedo", 1000, './media/peineHumedo.jpeg'))
-stockProductos.push(new Producto(26, "accesorios", "esponja", "vegetal", "exfoliante", "Esponja Vegetal Exfoliante", 450, './media/espVeg.jpeg'))
-stockProductos.push(new Producto(27, "accesorios", "cepillo", "madera", "exfoliante", "Cepillo de cuerpo Exfoliante", 800, './media/cepExf.jpeg'))
-stockProductos.push(new Producto(28, "accesorios", "cepillo", "bambu", "dental", "Cepillo de dientes", 400, './media/cepDental.jpeg'))
-stockProductos.push(new Producto(29, "accesorios", "porta cepillo", "bambu", "doble", "Porta Cepillo de dientes Doble", 800, './media/portaCep.jpeg'))
-stockProductos.push(new Producto(30, "accesorios", "porta cepillo", "bambu", "portable", "Estuche para Cepillo de Dientes", 900, './media/estucheCep.jpeg'))
-stockProductos.push(new Producto(31, "accesorios", "jabonera", "bambu", "doble", "Jabonera Doble", 700, './media/jabonera.jpeg'))
-stockProductos.push(new Producto(32, "otros", "almohadilla", "semillas", "chica", "Almohadilla de Semillas Chica", 1100, './media/almChica.jpeg'))
-stockProductos.push(new Producto(33, "otros", "almohadilla", "semillas", "grande", "Almohadilla de Semillas Grande", 1550, './media/almGrande.jpeg'))
 
-console.log(stockProductos)
+const stockProductos = []
+stockProductos.push(new Producto(1, "facial", "aguas", "aplicador", "rosas", "Agua de Rosas", 700, './media/aRosas.jpeg', 100))
+stockProductos.push(new Producto(2, "facial", "aguas", "aplicador", "hamamelis", "Agua de Hamamelis", 750, './media/aHamamelis.jpeg', 100))
+stockProductos.push(new Producto(3, "facial", "aguas", "aplicador", "azahar", "Agua de Azahar", 700, './media/aAzahar.jpeg', 100))
+stockProductos.push(new Producto(4, "capilar", "shampoo", "solido", "seco", "Shampoo Sólido para Cabello Seco", 900, './media/seco.jpeg', 150))
+stockProductos.push(new Producto(5, "capilar", "shampoo", "solido", "fortalecedor", "Shampoo Sólido Fortaleceer", 900, './media/fortalecedor.jpeg', 150))
+stockProductos.push(new Producto(6, "capilar", "shampoo", "solido", "equilibrado", "Shampoo Sólido Equilibrado", 900, './media/equilibrado.jpeg', 150))
+stockProductos.push(new Producto(7, "capilar", "shampoo", "solido", "graso", "Shampoo Sólido para Cabello Graso y Anti-Caspa", 900, './media/graso.jpeg', 150))
+stockProductos.push(new Producto(8, "capilar", "acondicionador", "solido", "acondicionador", "Acondicionador Sólido", 1000, './media/acondicionador.jpeg', 150))
+stockProductos.push(new Producto(9, "capilar", "serum", "aplicador", "serum", "Serum Capilar", 1100, './media/serum.jpeg', 100))
+stockProductos.push(new Producto(10, "corporal", "crema", "manos", "nutritiva", "Crema para Manos Nutritiva", 700, './media/creMan.jpeg', 100))
+stockProductos.push(new Producto(11, "corporal", "crema", "manos", "extra nutritiva", "Crema para Manos Extra-Nutritiva", 750, './media/creMan.jpeg', 100))
+stockProductos.push(new Producto(12, "corporal", "crema", "cuerpo", "nutritiva", "Crema Corporal Nutritiva", 1250, './media/creCor.jpeg', 100))
+stockProductos.push(new Producto(13, "corporal", "crema", "cuerpo", "extra nutritiva", "Crema Corporal Extra-Nutritiva", 1350, './media/creCorN.jpeg', 100))
+stockProductos.push(new Producto(14, "corporal", "jabon", "exfoliante", "mentolado", "Jabón Exfoliante Mentolado", 650, './media/jabonExf.jpeg', 300))
+stockProductos.push(new Producto(14, "corporal", "jabon", "esenciales", "tilo", "Jabón Esencial de Tilo", 400, './media/jabones.jpeg', 300))
+stockProductos.push(new Producto(15, "corporal", "jabon", "esenciales", "oleo 31", "Jabón Esencial de Oleo 31", 400, './media/jabones.jpeg', 300))
+stockProductos.push(new Producto(16, "corporal", "jabon", "esenciales", "coco", "Jabón Esencial de Coco", 400, './media/jabones.jpeg', 300))
+stockProductos.push(new Producto(17, "corporal", "jabon", "especiales", "calendula", "Jabón Esencial de Caléndula", 550, './media/jabCal.jpeg', 300))
+stockProductos.push(new Producto(18, "corporal", "jabon", "especiales", "jazmin", "Jabón Esencial de Jazmín", 5500, './media/jabJaz.jpeg', 300))
+stockProductos.push(new Producto(19, "corporal", "pasta dental", "vidrio", "eucalipto", "Pasta dental de Eucalipto", 550, './media/pastaDental.jpeg', 100))
+stockProductos.push(new Producto(20, "corporal", "pasta dental", "vidrio", "citrico", "Pasta dental Cítrica", 550, './media/pastaDental.jpeg', 100))
+stockProductos.push(new Producto(21, "corporal", "desodorante", "vidrio", "mix foral", "Desodorante Mix Floral", 600, './media/desodorante.jpeg', 150))
+stockProductos.push(new Producto(22, "corporal", "desodorante", "vidrio", "lavanda", "Desodorante Lavanda", 600, './media/desodorante.jpeg', 150))
+stockProductos.push(new Producto(23, "accesorios", "peines", "madera", "simple", "Peine para Cabello, Varios Modelos Simples", 650, './media/peineMadera.jpeg', 100))
+stockProductos.push(new Producto(24, "accesorios", "peines", "madera", "cabello seco", "Peine para Cabello Seco", 900, './media/peineSeco.jpeg', 50))
+stockProductos.push(new Producto(25, "accesorios", "peines", "madera", "cabello humedo", "Peine para Cabello Húmedo", 1000, './media/peineHumedo.jpeg', 50))
+stockProductos.push(new Producto(26, "accesorios", "esponja", "vegetal", "exfoliante", "Esponja Vegetal Exfoliante", 150, './media/espVeg.jpeg', 200))
+stockProductos.push(new Producto(27, "accesorios", "cepillo", "madera", "exfoliante", "Cepillo de cuerpo Exfoliante", 800, './media/cepExf.jpeg', 100))
+stockProductos.push(new Producto(28, "accesorios", "cepillo", "bambu", "dental", "Cepillo de dientes", 400, './media/cepDental.jpeg', 300))
+stockProductos.push(new Producto(29, "accesorios", "porta cepillo", "bambu", "doble", "Porta Cepillo de dientes Doble", 800, './media/portaCep.jpeg', 100))
+stockProductos.push(new Producto(30, "accesorios", "porta cepillo", "bambu", "portable", "Estuche para Cepillo de Dientes", 900, './media/estucheCep.jpeg', 200))
+stockProductos.push(new Producto(31, "accesorios", "jabonera", "bambu", "doble", "Jabonera Doble", 700, './media/jabonera.jpeg', 150))
+stockProductos.push(new Producto(32, "otros", "almohadilla", "semillas", "chica", "Almohadilla de Semillas Chica", 1100, './media/almChica.jpeg', 50))
+stockProductos.push(new Producto(33, "otros", "almohadilla", "semillas", "grande", "Almohadilla de Semillas Grande", 1550, './media/almGrande.jpeg', 50))
